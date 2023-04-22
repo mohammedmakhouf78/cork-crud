d880039fddccc891c3224b8ae35b4916a649ba73
mohammedmakhlouf78 <mohammedmakhlouf78@gmail.com>
2023-04-23 00:59:15 +0200

    udpates

--- src/Services/IndexViewService.php
+++ src/Services/IndexViewService.php
@@ -15,6 +15,7 @@ class IndexViewService extends AbstractParent
 
     public function prepareStub()
     {
+        $tableName = preg_replace('/([A-Z])/', '-$1', $this->modelLower);
         $this->stub = str_replace("{model}", $this->model, $this->stub);
     }
 
